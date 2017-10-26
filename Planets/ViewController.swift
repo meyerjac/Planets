6bf770a6f5539f7df4e9c01a4fb6985512e7f0f9
jackson meyer <jackson.meyer7@gmail.com>
2017-10-25 22:33:06 -0700

    added an earth with a diffuse, specular, and emission layer

--- Planets/ViewController.swift
+++ Planets/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        
+        self.sceneView.session.run(configuration)
+        self.sceneView.automaticallyUpdatesLighting = true
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        let earth = SCNNode()
+        earth.geometry = SCNSphere(radius: 0.05)
+        earth.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "earthDay")
+        earth.geometry?.firstMaterial?.specular.contents = #imageLiteral(resourceName: "earthSpecular")
+        earth.geometry?.firstMaterial?.emission.contents = #imageLiteral(resourceName: "earthEmission")
+        earth.position = SCNVector3(0,0,-1.0)
+        self.sceneView.scene.rootNode.addChildNode(earth)
+        
+        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
+        let forever = SCNAction.repeatForever(action)
+        earth.runAction(forever)
+     
+    }
 
+}
+
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi/180}
 }
 
